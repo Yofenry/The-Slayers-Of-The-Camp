3a801e21057fbfbc25c949d8eb0dd3b94685c004
Yofenry <72547311+Yofenry@users.noreply.github.com>
2020-11-28 09:26:12 +1000

    Add files via upload

---
+++ The Slayers Of The Camp.py
@@ -0,0 +1,8 @@
+name = input("Whats Your name ")
+xp = 0
+health = 100
+wpn = input("type in a weapon u want to use ")
+print("Good choice! " + name)
+print("You Killed 5 Goblins And You Know Have 10 Xp")
+xp = 10
+drgnnam=input("You Found A Dragon Now Name It ")
